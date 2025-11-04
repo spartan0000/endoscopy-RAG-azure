13c02479c244a83c231c30c13fd6328863f29d0b
David Lee <doctor.dlee@gmail.com>
2025-11-04 22:01:31 +1300

    add documents and functions

--- clients.py
+++ clients.py
@@ -1,3 +1,18 @@
 from openai import OpenAI, AzureOpenAI
 import os
+import load_dotenv
+
+load_dotenv()
+
+chat_client = AzureOpenAI(
+    api_version = os.getenv('AZURE_GPT_API_VERSION'),
+    api_key = os.getenv('AZURE_OPENAI_API_KEY'),
+    azure_endpoint = os.getenv('AZURE_ENDPOINT'),
+)
+
+embedding_client = AzureOpenAI(
+    api_version = os.getenv('AZURE_EMBEDDING_API_VERSION'),
+    api_key = os.getenv('AZURE_OPENAI_API_KEY'),
+    azure_endpoint = os.getenv('AZURE_ENDPOINT'),
+)
 
--- functions.py
+++ functions.py
@@ -8,11 +8,113 @@ import json
 from dotenv import load_dotenv
 import langchain
 
+import asyncio
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 import logging
 from datetime import datetime
 
+from clients import chat_client, embedding_client
 
 load_dotenv()
+
+async def format_query_json(user_query: str) -> dict: 
+    system_prompt = """
+    summarize the user input that includes medical data on a person's history of colonoscopy procedures and the pathology reports from the polyps that were removed.
+    key information includes the following:
+    - dates of the procedure including month, day, and year.
+    - the number of polyps
+    - the size of the polyps which is generally reported in millimeters - less than 10mm or greater than or equal to 10mm is a useful cutoff
+    - the histology of the polyps.  
+    - for adenomas - the number of polyps and the size of the largest adenoma, whether there is high grade dysplasia (yes/no), whether the adenoma is tubulovillous or villous (yes/no)
+    - for sessile serrated polyps - the number of sessile serrated polyps, the size of the largest sessile serrated polyp, whether there is dysplasia (yes/no)
+    - for hyperplastic polyps - size greater than or equal to 10mm (yes/no)
+    format the output as ***JSON output*** with the following schema for each colonoscopy procedure.  
+
+    {'patient_name': '',
+    'patient_NHI': '',
+    
+        'colonoscopy': [
+                            {'date': '', 
+                            'number of polyps': 0, 
+                            },
+        'histology': {
+            {'adenoma': number of adenomas,
+            'adenoma_size': 'largest adenoma size in mm',
+            'high_grade_dysplasia_in the adenoma': 'yes' or 'no',
+            'tubulovillous_or_villous_adenoma': 'yes' or 'no',
+            'sessile_serrated_polyps': number of sessile serrated polyps,
+            'sessile_serrated_polyp_size': 'size of largest sessile serrated polyp',
+            'dysplasia_in_the_sessile_serrated_polyp': 'yes' or 'no',
+            'hyperplastic_polyp_greater_or_equal_to_10mm_in_size: 'yes' or 'no',
+        }
+    }
+    ]
+}
+    make sure the JSON is properly formatted and can be parsed by a standard JSON parser.
+"""
+
+
+    user_prompt = f'Please format this medical text into structured JSON output - {user_query}'
+
+    response1 = await chat_client.responses.create(
+        model = 'gpt-4o-mini',
+        text = {'format': {'type': 'json_object'}},
+        input = [
+            {
+                'role':'system',
+                'content': system_prompt,
+            },
+            {
+                'role': 'user',
+                'content': user_prompt,
+            }
+        ],
+        
+
+    )
+
+    try:
+        raw_output = response1.output_text
+        result_json = json.loads(raw_output)
+        return result_json
+    except json.JSONDecodeError:
+        return {'error': 'Failed to parse JSON', 'raw_output': response1.output_text}
+
+    
+
+
+async def format_query_summary(user_query: str) -> str:
+    system_prompt = """
+    you are a helpful medical assistant who is tasked with providing a detailed summary of only the pertinent details of the user input data that references recent colonoscopy procedures,
+    the details from the procedure notes themselves, as well as the histological report from any polyps that were removed during that procedure.  Pertinent information that must be included
+    in the summary are the number of polyps, the types of polyps (such as adenoma and whether the adenoma is tubulovillous or villous) , sessile serrated polyps, hyperplastic polyps as well as their sizes. 
+    Regarding the procedure details, the significant findings include the BBPS score and where the scope was advanced to.  Regarding the polyps that are noted, please summarize and reconcile the information
+    on the polyps that is contained within the procedure note as well as the histology report.  DO NOT make any clinical diagnoses or recommendations.
+    """
+
+    response = await chat_client.responses.create(
+        model = 'gpt-5-mini',
+        input = [
+            {'role': 'system',
+             'content': system_prompt,
+             },
+             {'role': 'user',
+              'content': user_query}
+              
+        ],
+        
+    )
+
+    return response.output_text
+    
+
+async def get_embedding(text: str) -> List[float]:
+    response = await embedding_client.embeddings.create(
+        input = text,
+        model = 'text-embedding-3-small',
+    )
+    return response.data[0].embedding
+
 
